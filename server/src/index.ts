120d9155dfe1aa4815077ed72425410bc23b2b86
sonplusone <son.nguyen@plusonerobotics.com>
2022-03-16 23:14:35 -0500

    Remove monorepo because of the docker-compose

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get("/", function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
+  res.json({ msg: "This is CORS-enabled for all origins!!!" });
   next();
 });
 
